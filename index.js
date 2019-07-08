3b643c38dec7a03f4c560f9b98e93f68fbf15dbc
Yusuf Abdulkarim <haywhyze@gmail.com>
2019-07-08 14:34:30 +0100

    feat: handle error on saving new user

--- index.js
+++ index.js
@@ -11,15 +11,28 @@ app.get('/', (req, res) => {
 });
 
 app.post('/api/users', (req, res) => {
+
+  const { name, bio } = req.body;
+
+  if (!name || !bio ) {
+    return res.status(400).send({
+      errorMessage: 'Please provide name and bio for the user.',
+    });
+  }
+
   const newUser = {
     name: req.body.name,
     bio: req.body.bio,
   }
+
   userModel.insert(newUser).then(data => {
     res.status(201).json(data)
   }).catch((error => {
-    console.log(error)
-  }))
+    console.log(error);
+    return res.status(500).json({ 
+      error: 'There was an error while saving the user to the database'
+    });
+  }));
 })
 
 app.get('/api/users', (req, res) => {
