b2d17a560424cbb76bc4513e3df9372c63d6355b
Yusuf Abdulkarim <haywhyze@gmail.com>
2019-07-08 13:09:44 +0100

    feat: install express create express app

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // implement your API here
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World')
+});
+
+app.listen(1900, () => console.log('app listening on port 1900'));
