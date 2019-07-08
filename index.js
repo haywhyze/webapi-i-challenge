5a89181b74816643c48eafb04bb77aa451c3f3f6
Yusuf Abdulkarim <haywhyze@gmail.com>
2019-07-08 13:27:00 +0100

    feat: implement get all users endpoint

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 // implement your API here
 const express = require('express');
-
+const userModel = require('./data/db');
 const app = express();
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
-  res.send('Hello World')
+  res.send('Welcome to Lambda Users API')
+});
+
+app.get('/api/users', (req, res) => {
+  userModel.find()
+    .then(data => {
+      res.status(200).json(data)
+    }).catch(error => {
+      res.status(500).json(error)
+    })
 });
 
 app.listen(1900, () => console.log('app listening on port 1900'));
